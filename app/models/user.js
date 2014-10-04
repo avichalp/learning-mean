aa354d8e21ec16c758924d4f8387e62b62af1833
avichalp <pandey_avichal@yahoo.com>
2014-10-05 03:09:23 +0530

    cleaning backend code

--- app/models/user.js
+++ app/models/user.js
@@ -1,24 +1,25 @@
-var mongoose = require( 'mongoose' );
-var bcrypt   = require('bcrypt-nodejs');
+(function () {
 
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-       
-	local : {	    
-	    email : String,	    
-	    password : String,
-	} 
-			
-    });
+    var bcrypt   = require('bcrypt-nodejs');
+    var mongoose = require( 'mongoose' );
     
-// methods
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
+    var Schema = mongoose.Schema;    
+    var userSchema = new Schema({
+	    local : {
+		email : String,	    
+		password : String,
+	    } 			
+	});
+    
+    // methods
+    userSchema.methods.generateHash = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    };
 
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
+    userSchema.methods.validPassword = function(password) {
+	return bcrypt.compareSync(password, this.local.password);
+    };
 
-module.exports = mongoose.model( 'User' , userSchema );
+    module.exports = mongoose.model( 'User' , userSchema );
+
+})();
