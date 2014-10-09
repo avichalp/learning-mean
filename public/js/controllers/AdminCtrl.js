53e4c66d079e553f6037bf451608ff4861790ae5
avichalp <pandey_avichal@yahoo.com>
2014-10-10 01:03:45 +0530

    form validation complete

--- public/js/controllers/AdminCtrl.js
+++ public/js/controllers/AdminCtrl.js
@@ -20,68 +20,69 @@ function AdminController($scope, Logout, Admin, Main, About, Contact, Product) {
 			 });
     };
 		    
-    //try ng-init ?
-    $scope.homeForm = {};
-    $scope.homeForm.homeText = '';
-    $scope.homeForm.submit = function () {
-		    
-	var homeText = $scope.homeForm.homeText;
+    $scope.home = {
 	
-	Main.postHome(
-	    { 
-		homeText: homeText
-	    },
-	    function (data) {
-		if (data)
-		    window.location = "http://localhost:8080/";
-	    });
-		    
+	submit : function (isValid) {
+	    if (isValid){
+		Main.postHome(
+		    { 
+			homeText: $scope.home.homeText
+		    },
+		    function (data) {
+			if (data)
+			    window.location = "http://localhost:8080/";
+		    });
+	   }
+	}		    
     };
 		
-    $scope.aboutForm = {};
-    $scope.aboutForm.aboutText = '';
-    $scope.aboutForm.submit = function () {
+    $scope.about = {
 	
-	var aboutText = $scope.aboutForm.aboutText;
-	About.postAbout(
-	    { 
-		aboutText: aboutText
-	    },
-	    function (data){
-		if (data)
-		    window.location = "http://localhost:8080/about";
-	    });
+	submit : function (isValid) {
+	    if (isValid){			    
+		About.postAbout(
+		    { 
+			aboutText: $scope.about.aboutText
+		    },
+		    function (data){
+			if (data)
+			    window.location = "http://localhost:8080/about";
+		    });
+	   } 
+	}
     };
     
-    $scope.contactForm = {};
-    $scope.contactForm.contactText = '';
-    $scope.contactForm.submit = function () {
+    $scope.contact = {
+	    
+	submit : function (isValid) {
+	    if (isValid){
+		Contact.postContact(
+		    {
+			contactText : $scope.contact.contactText
+		    },
+		    function (data) {
+			if (data)
+			    window.location = "http://localhost:8080/contact";
+		    });
+	   } 
+	}
+    };	
+
+    $scope.product = {
 	
-	var contactText = $scope.contactForm.contactText;			
-	Contact.postContact(
-	    {
-		contactText : contactText
-	    },
-	    function (data) {
-		if (data)
-		    window.location = "http://localhost:8080/contact";
-	    });
-    };
-		
-    $scope.productForm = {};
-    $scope.productForm.productText = '';
-    $scope.productForm.submit = function () {
-		    
-	var productText = $scope.productForm.productText;			
-	Product.postProduct(
-	    {
-		productText : productText
-	    },
-	    function (data) {
-		if (data)
-		    window.location = "http://localhost:8080/product";
-	    });
-    };
+	submit : function (isValid) {
+	    if (isValid){  
+		Product.postProduct(
+		    {
+			productText : $scope.product.productText
+		    },
+		    function (data) {
+			if (data)
+			    window.location = "http://localhost:8080/product";
+		    });
+	    }
+	}
+    };    
 }
 
 angular
