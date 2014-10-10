7868390da66acf17a7cccbc03538d49969adc888
avichalp <pandey_avichal@yahoo.com>
2014-10-10 20:23:57 +0530

    seprated content rest apsis

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 // configuring redis
 var redis = require('redis');
 var client = redis.createClient();
-
 // including modles
 var User = require('./models/user');
     
@@ -16,41 +15,113 @@ function isLoggedIn(req, res, next) {
  
 // fuction for maping GET/POST/DELETE on Content    
 function routes(app, passport) {
-	    		   
-    function apiCall(apiType) {	
-			       
-	app.route('/api/' + apiType + '/' )
+
+    return function () {
+			
+	app.route('/api/home/')
 	    .get(function (req, res){
-		     client.get(apiType + 'key', function (err, reply) {
+		     client.get('homeKey',
+				function (err, reply) {
 				    if (err)
 					console.log(err);
 				    res.json(reply);
 				});
-		 })		
+		 })			
 	    .post(isLoggedIn, function (req, res) {
 		      console.log(req.body);
-		      client.set( apiType + 'key', req.body[apiType], function (err, reply) {
-				      if (err)
-					  console.log(err);
-				      res.json(reply);
-				  });		
+		      client.set('homeKey',
+				 req.body['home'], function (err, reply) {
+				     if (err)
+					 console.log(err);
+				     res.json(reply);
+				 });
 		  })
 	    .delete(isLoggedIn, function(req, res){
-			client.del(apiType + 'key', function (err, reply) {
+			client.del('homeKey',
+				   function (err, reply) {
 				       if (err)
 					   res.json(reply);
 				   });
-					       
 		    });
-			       
-    };
-       	    
-    // return function that maps urls with methods
-    function urlMapping() {
-	
-	// url maps to GET/POST/DELETE content sections
-	['home', 'about', 'product', 'contact' ].map(apiCall);
-		   
+
+	app.route('/api/about/')
+	    .get(function (req, res){
+		     client.get('aboutKey',
+				function (err, reply) {
+				    if (err)
+					console.log(err);
+				    res.json(reply);
+				});
+		 })			
+	    .post(isLoggedIn, function (req, res) {
+		      console.log(req.body);
+		      client.set('aboutKey',
+				 req.body['about'], function (err, reply) {
+				     if (err)
+					 console.log(err);
+				     res.json(reply);
+				 });
+		  })
+	    .delete(isLoggedIn, function(req, res){
+			client.del('aboutKey',
+				   function (err, reply) {
+				       if (err)
+					   res.json(reply);
+				   });
+		    });
+
+	app.route('/api/contact/')
+	    .get(function (req, res){
+		     client.get('contactKey',
+				function (err, reply) {
+				    if (err)
+					console.log(err);
+				    res.json(reply);
+				});
+		 })			
+	    .post(isLoggedIn, function (req, res) {
+		      console.log(req.body);
+		      client.set('contactKey',
+				 req.body['contact'], function (err, reply) {
+				     if (err)
+					 console.log(err);
+				     res.json(reply);
+				 });
+		  })
+	    .delete(isLoggedIn, function(req, res){
+			client.del('contactKey',
+				   function (err, reply) {
+				       if (err)
+					   res.json(reply);
+				   });
+		    });
+
+	app.route('/api/product/')
+	    .get(function (req, res){
+		     client.get('productKey',
+				function (err, reply) {
+				    if (err)
+					console.log(err);
+				    res.json(reply);
+				});
+		 })			
+	    .post(isLoggedIn, function (req, res) {
+		      console.log(req.body);
+		      client.set('productKey',
+				 req.body['product'], function (err, reply) {
+				     if (err)
+					 console.log(err);
+				     res.json(reply);
+				 });
+		  })
+	    .delete(isLoggedIn, function(req, res){
+			client.del('productKey',
+				   function (err, reply) {
+				       if (err)
+					   res.json(reply);
+				   });
+		    });
+
 	// url map to POST login details
 	app.post('/api/login/' ,passport.authenticate('local-login'), function (req, res) {
 		     res.json({message: 'OK'});
@@ -63,13 +134,13 @@ function routes(app, passport) {
 	app.post('/api/addadmin/',isLoggedIn, passport.authenticate('local-signup'), function (req, res) {
 		     res.json( {message: 'OK'} );
 		 });
-
+	
 	// url  map to GET list of admins 
 	app.get('/api/adminlist/', isLoggedIn, function (req, res) {
 		    User.find(function (err, admins) {
 				  if (err)
 				      res.send(err);
-				 
+				  
 				  var adminList = {};
 				  
 				  for (var i =0; i<= admins.length -1; i++)
@@ -80,9 +151,8 @@ function routes(app, passport) {
 			     
 			     );
 		    
-		});
-    
-				       
+		});   
+	
 	// url map to GET  admin page
 	app.get('/api/admin/', isLoggedIn, function (req, res) {
 		    res.json( {message: 'OK'} );
@@ -105,13 +175,10 @@ function routes(app, passport) {
 	// default GET
 	app.get('*', function (req, res) {
 		    res.sendfile('./public/index.html');
-		});	
-				       
-    };    
-			
-    return urlMapping;
-}
-
+		});
+    };
+}    
+	    		            	    
 module.exports = routes;
 
  
